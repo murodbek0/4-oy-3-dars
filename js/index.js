ca38744a85ca5a7ad4f3b4a4c0d788c83f34cec2
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-07-19 23:09:22 +0500

    examples

---
+++ js/index.js
@@ -0,0 +1,243 @@
+// if (\_/)
+
+// 1 misol
+
+// let age = prompt('yosh krting');
+// let res = '';
+// if(age > 18){
+//   res = 'siz katta yoshdagizsiz'
+// }else if( age < 18){
+//   res = 'siz hali yoshsiz';
+
+// }
+// console.log(res);
+
+// 2 misol
+
+// let res = prompt('son krting')
+// res = parseInt(res);
+// if( res % 2 === 0 ){
+//  console.log('juft');
+// }else {
+// console.log('toq');
+// }
+
+// 3 misol
+// let res = prompt('son krting')
+// res = parseInt(res);
+// if( res >=0 ){
+//  console.log('manfiy');
+// }else {
+// console.log('musbat');
+// }
+
+// 4 misol
+// let age = prompt('harorat krting');
+// let res = '';
+// if(age >= 20){
+//   res = 'yengl kiying'
+// }else if( age <= 20){
+//   res = 'qalin keyin';
+
+// }
+// console.log(res);
+
+// 5 misol
+// let sum = prompt("ball krting");
+// let res = "";
+// if (sum > 90) {
+//   res = "alo";
+// } else if (sum > 70-89) {
+//   res = "yaxshi";
+// } else if (sum > 50-69) {
+//   res = "qoniqarli";
+// } else if (sum < 50) {
+//   res = "yomon";
+// }
+// console.log(res);
+
+// 6 misol
+
+// let name = prompt('ism krting');
+// if (name === 'ali'){
+//  console.log('salom ali');
+
+// }else{
+//  console.log('salom mehmon');
+// }
+
+// 7 misol
+
+// let sum = prompt('summa krting');
+// let res = '';
+// if(sum > 100){
+//   res = 'Chegirma qollanildi'
+// }else if( sum < 100){
+//   res = 'Chegirma mavjud emas';
+
+// }
+// console.log(res);
+
+
+// Ternary (\_/)
+
+// 1 misol
+// let tem = prompt('harorat krting');
+// tem = parseFloat(tem);
+// let result = (tem > 25)? 'issiq' : 'sovuq';
+// alert(result);
+
+// 2 misol
+// let sum = prompt('ball krting');
+// sum = parseFloat(sum);
+// let result = (sum >= 50)? 'otdi' : 'yeqildi';
+// alert(result);
+
+// 3 misol
+// let sum = prompt('son krting');
+// sum = parseFloat(sum);
+// let result = (sum > 0)? 'manfiy' : 'musbat';
+// alert(result);
+
+// for (\_/)
+
+// 1 misol
+
+// for(let i = 1 ; i <= 10; i++){
+//     console.log(i);
+
+// }
+
+// 2 misol
+// for(let i = 10; i >= 1; i--){
+//     console.log(i);
+
+// }
+
+// 3 misol
+// for(let i = 2; i <=100; i+=2){
+//     console.log(i);
+
+// }
+
+// 4 misol
+
+// for(let i = 1; i <=100; i+=2){
+//     console.log(i);
+
+// }
+
+// 5 misol
+
+// let sum = 0;
+//  for(let i = 1; i <=10; i++){
+//     sum = sum + i
+
+// }
+// console.log(sum);
+
+// 6 misol
+// let sum = 1;
+//  for(let i = 1; i <=10; i++){
+//     sum = sum * i
+// }
+// console.log(sum);
+
+// 7 misol
+
+// 8 misol
+
+//  for(let i = 1; i <= 50; i++){
+//     console.log(i * i);
+// }
+
+// 9 misol
+
+//  for(let i = 1; i <= 20; i++){
+//     cube = i * i * i;
+//     console.log(cube);
+// }
+
+// 10 misol
+// let sum =0 ;
+//  for(let i = 1; i <= 1000; i++){
+//     sum = sum + i
+// }
+// console.log(sum);
+
+// 11 misol
+
+// for(let i = 0; i < 5; i++){
+// console.log('salom');
+// }
+
+// 12 misol
+
+// for (let i = 1; i <= 100;  i++){
+// let result = i * 2;
+// console.log(result);
+// }
+
+// 13 misol
+
+// 14 misol
+
+//  for(let i = 1; i <= 10; i++){
+//     console.log(i * i);
+// }
+
+// 15 misol
+// for(let i = 97 ; i <= 122; i++){
+//     console.log(String.fromCharCode(i));
+
+// }
+
+// Boolean (\_/)
+
+// 1 misol
+// let sum = prompt(' 1 sonni krting')
+// let sum1 = prompt('2 sonni krting')
+
+// if ((sum % 2 === 0 && sum1 % 2 === 0) || (sum % 2 !== 1 && sum1 % 2 !== 1)) {
+//   console.log("juft"); 
+// } else {
+//   console.log("toq"); 
+// }
+
+// 2 misol
+// let a = 8
+// let b = 10
+// let c = 15
+
+//  let res = (a > 0 && b >0 && c >0);
+//  console.log(res);
+ 
+// 3 misol
+
+// let a = prompt('1 son krting');
+// let b = prompt('2 son krting');
+// let c = prompt('3 son krting');
+
+//  let res = (a < 0 || b < 0 || c > 0 && a > 0 || b < 0 || c < 0 && a < 0 || b > 0 || c < 0);
+//  console.log(res);
+
+// 4 misol
+// let a = -8
+// let b = -10
+// let c = 15
+
+//  let res = (a < 0 && b <0 && c >0);
+//  console.log(res);
+
+// 5 misol
+// let a = -8
+// let b = 10
+// let c = 15
+
+//  let res = (a < 0 && b >0 && c >0);
+//  console.log(res);
+
+// 6 misol
+// let a = 20;
+// let res = (a > 0);
+// console.log(res);
